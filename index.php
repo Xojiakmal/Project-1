26394aff969a5b36e759eb7c00cd31768d8db171
Xojiakmal <xojiakmal001@gmail.com>
2025-05-21 14:23:29 +0500

    aisdi

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+$arr = ['bir', 'ikki', 'uch'];
+
+$myJson = json_encode($arr);
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,13 +24,15 @@
     </style>
 </head>
 <body>
-    <form action="index.html" method="post" onsubmit="return abd()" name="form">
+    <form action="" method="post" onsubmit="return abd()" name="form">
         <div id="son1"></div>
         <div id="bel">+</div>
         <div id="son2"></div>
         =
         <input type="tel" id="jav" name="jav"><br>
         <button type="submit">Yuborish</button>
+        <br>
+        <div id="xato"></div>
     </form>
 
     <script src="script.js"></script>
--- script.js
+++ script.js
@@ -1,22 +1,14 @@
 // document.getElementById('son1').innerHTML = 6;
 document.getElementById('son2').innerHTML = 4;
 
-const xmlhttp = new XMLHttpRequest();
-
-xmlhttp.onload = function() {
-  const myObj = JSON.parse(this.responseText);
-  document.getElementById("demo").innerHTML = myObj.name;
-}
-xmlhttp.open("GET", "index.php");
-xmlhttp.send();
-
-
 son()
 function abd() {
     let X = document.forms['form']['jav'].value;
     if (X == "") {
         document.getElementById('jav').style.borderColor='red';
-        return false;   
+        document.getElementById('xato').style.color='red';
+        document.getElementById('xato').innerHTML = "Javob kiritmadingiz";
+        return false;
     }
 }
 function son() {
@@ -25,6 +17,5 @@ function son() {
         document.getElementById('son1').innerHTML = son1;
         let son2 = Math.floor(Math.random()*(10));
         document.getElementById('son2').innerHTML = son2;
-        
     }
 }
