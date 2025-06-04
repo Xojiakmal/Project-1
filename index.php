4b3625a927ee816b8086c2df100bb2c7b5bc49f8
Xojiakmal <xojiakmal001@gmail.com>
2025-06-04 11:47:47 +0500

    midnc

--- index.php
+++ index.php
@@ -4,11 +4,9 @@ if (!isset($_SESSION['lvl'])) {
     $_SESSION['lvl'] = 1;
     $_SESSION['son1'] = rand(1,30);
     $_SESSION['son2'] = rand(1,30);
-    $_SESSION['bel'] = rand(1,3);
+    $_SESSION['bel'] = rand(1,2);
 }
-
-if (isset($_GET['jav'])) {
-
+if (isset($_POST['jav'])) {
     if (tek()) {
         if ($_SESSION['lvl'] <= 5) {
             $_SESSION['son1'] = rand(1,30);
@@ -29,7 +27,6 @@ if (isset($_GET['jav'])) {
                 $_SESSION['son1'] = rand(1,50);
                 $_SESSION['son2'] = rand(1,5);
             }
-
         }else/*if ($_SESSION['lvl'] >= 10)*/ {
             $_SESSION['bel'] = rand(1,4);
             if ($_SESSION['bel'] == 1 or $_SESSION['bel'] == 2) {
@@ -46,35 +43,29 @@ if (isset($_GET['jav'])) {
             }
         }
         $_SESSION['lvl']++;
-        
-        // header("Location: ".$_SERVER['PHP_SELF']);
-        // exit;
     }
-    elseif ($_GET['jav'] == '') {
+    elseif ($_POST['jav'] == '') {
         $xato = "Javob kiritmadingiz";
     }
     else {
         $xato = "To'g'ri javob kirirng";
     }
 }
-// $_SESSION['belgi'] = belgi($_SESSION['bel']);
-
-
 function tek() {
     if ($_SESSION['bel'] == 1) {
-        if (($_SESSION['son1'] + $_SESSION['son2']) == $_GET['jav']) {
+        if (($_SESSION['son1'] + $_SESSION['son2']) == $_POST['jav']) {
             return true;
         }
     }elseif ($_SESSION['bel'] == 2) {
-        if (($_SESSION['son1'] - $_SESSION['son2']) == $_GET['jav']) {
+        if (($_SESSION['son1'] - $_SESSION['son2']) == $_POST['jav']) {
             return true;
         }
     }elseif ($_SESSION['bel'] == 3) {
-        if (($_SESSION['son1'] * $_SESSION['son2']) == $_GET['jav']) {
+        if (($_SESSION['son1'] * $_SESSION['son2']) == $_POST['jav']) {
             return true;
         }
     }elseif ($_SESSION['bel'] == 4) {
-        if (round($_SESSION['son1'] / $_SESSION['son2']) == $_GET['jav']) {
+        if (round($_SESSION['son1'] / $_SESSION['son2']) == $_POST['jav']) {
             return true;
         }
     }
@@ -90,7 +81,7 @@ function belgi($bel) {
         return(':');
     }
 }
-print_r($_SESSION);
+// print_r($_SESSION);
 // print_r($_POST);
 // session_destroy()
 ?>
@@ -100,32 +91,23 @@ print_r($_SESSION);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>
-        div{
-            display:inline-block;
-        }
-        body{
-            background: #000;
-            color: white;
-        }
-        input{
-            width: 20px;
-        }
-    </style>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <form action="" method="GET" onsubmit="return abd()" name="form">
-        <h2><?=$_SESSION['lvl']?> Level</h2>
-        <div id="son1"><?=$_SESSION['son1']?></div>
-        <div id="bel"><?=belgi($_SESSION['bel'])/* echo $_SESSION['bel']*/?></div>
-        <div id="son2"><?=$_SESSION['son2']?></div>
-        =
-        <input type="tel" id="jav" name="jav"><br>
-        <button type='submit'>Yuborish</button>
-        <br>
+    <div class="container">
+        <h2 class='lvl'><?=$_SESSION['lvl']?> Level</h2>
+        <form action="" method="POST">
+            <div class="bolim">
+                <div id="son1"><?=$_SESSION['son1']?></div>
+                <div id="bel"><?=belgi($_SESSION['bel'])?></div>
+                <div id="son2"><?=$_SESSION['son2']?></div>
+                =
+                <input type="tel" id="jav" name="jav"><br>
+            </div>
+            <br>
+            <button type='submit'>Yuborish</button>
+        </form>
         <h2 id="xato"><?=$xato?></h2>
-    </form>
-
-    <script src="script.js"></script>
+    </div>
 </body>
 </html>
